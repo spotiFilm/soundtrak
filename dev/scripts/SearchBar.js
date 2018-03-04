e95f7b224552e0e2bf914cd580b70985024bf707
Mark Visperas <markvisperas@hotmail.com>
2018-03-04 16:55:12 -0500

    movie selection styles

---
+++ dev/scripts/SearchBar.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import axios from 'axios';
+
+const apiURL = 'https://api.themoviedb.org/3'
+const apiKey = 'ba4403ee3098a16bd3c83fc121edf709'
+
+
+
+export default SearchBar;
